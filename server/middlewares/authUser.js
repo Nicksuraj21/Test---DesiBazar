dd0257dba6131b3d8b519ce602326d340bcfd20d
Nicksuraj21 <nicksuraj21@gmail.com>
2026-02-11 12:04:12 +0530

    changes fix all

--- server/middlewares/authUser.js
+++ server/middlewares/authUser.js
@@ -1,24 +1,57 @@
+// import jwt from 'jsonwebtoken';
+
+// const authUser = async (req, res, next)=>{
+//     const {token} = req.cookies;
+
+//     if(!token){
+//         return res.json({ success: false, message: 'Not Authorized' });
+//     }
+
+//     try {
+//         const tokenDecode = jwt.verify(token, process.env.JWT_SECRET)
+//         if(tokenDecode.id){
+//             req.body.userId = tokenDecode.id;
+//         }else{
+//             return res.json({ success: false, message: 'Not Authorized' });
+//         }
+//         next();
+
+//     } catch (error) {
+//         res.json({ success: false, message: error.message });
+//     }
+// }
+
+// export default authUser;
+
+
+
+
+
+
 import jwt from 'jsonwebtoken';
 
-const authUser = async (req, res, next)=>{
-    const {token} = req.cookies;
+const authUser = async (req, res, next) => {
+    const { token } = req.cookies;
 
-    if(!token){
+    if (!token) {
         return res.json({ success: false, message: 'Not Authorized' });
     }
 
     try {
-        const tokenDecode = jwt.verify(token, process.env.JWT_SECRET)
-        if(tokenDecode.id){
-            req.body.userId = tokenDecode.id;
-        }else{
+        const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+
+        if (tokenDecode.id) {
+            req.userId = tokenDecode.id;   // ✅ FIXED (important)
+            next();
+        } else {
             return res.json({ success: false, message: 'Not Authorized' });
         }
-        next();
 
+
+        
     } catch (error) {
-        res.json({ success: false, message: error.message });
+        return res.json({ success: false, message: error.message });
     }
-}
+};
 
 export default authUser;
