764ef249b85daedd302de369d86d9fcae48f36e5
Nicksuraj21 <nicksuraj21@gmail.com>
2026-02-28 01:48:17 +0530

    modify-catogary

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -20,7 +20,7 @@ const Categories = () => {
             }}
             >
                 <img src={category.image} alt={category.text} className='group-hover:scale-108 transition max-w-28'/>
-                <p className='text-sm font-medium'>{category.text}</p>
+                {/* <p className='text-sm font-medium'>{category.text}</p> */}
             </div>
                     
         ))}
