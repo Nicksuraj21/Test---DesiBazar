6a171f4afc9705294d084ec67fb53c5398b619af
Nicksuraj21 <nicksuraj21@gmail.com>
2026-03-04 14:17:33 +0530

    Optimeze for fast load

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,61 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+//   ],
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-  ],
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.destination === "image",
+            handler: "CacheFirst",
+
+            options: {
+              cacheName: "image-cache",
+
+              expiration: {
+                maxEntries: 200,
+                maxAgeSeconds: 60 * 60 * 24 * 30
+              }
+            }
+          }
+        ]
+      }
+
+    })
+
+  ]
 })
