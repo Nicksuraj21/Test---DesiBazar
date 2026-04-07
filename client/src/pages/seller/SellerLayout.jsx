898a486b2c5b2ddce1260b39f374cba64076067b
Nicksuraj21 <nicksuraj21@gmail.com>
2026-04-07 12:28:26 +0530

    seller panel me logo se home page redirect remove

--- client/src/pages/seller/SellerLayout.jsx
+++ client/src/pages/seller/SellerLayout.jsx
@@ -33,7 +33,8 @@ const SellerLayout = () => {
     return (
         <>
             <div className="flex items-center justify-between px-4 md:px-8 border-b border-gray-300 py-3 bg-white">
-                <Link to='/'>
+                <Link>
+                {/* <Link to='/'>  Home page redirect*/}
                     <img src={assets.logo} alt="log" className="cursor-pointer w-34 md:w-38" />
                 </Link>
                 <div className="flex items-center gap-5 text-gray-500">
